1af5d89945a73548424d06ba31a40a196ae3939f
Masrafi <masrafianam@gmail.com>
2024-05-14 21:47:06 +0600

    Initial Commit

---
+++ atB Jobs iOS/atB_Jobs_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  atB_Jobs_iOSApp.swift
+//  atB Jobs iOS
+//
+//  Created by Md Khorshed Alam on 14/5/24.
+//
+
+import SwiftUI
+
+@main
+struct atB_Jobs_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
